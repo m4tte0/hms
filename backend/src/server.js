739b3005f4c19c27856bf65e97d3f0c41660823e
mathon <matteo.honfucci@gmail.com>
2026-01-02 16:44:19 +0100

    upd: major upgrade
    mathon - jan 2, 2026
    ---
    </>

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,7 @@ const fs = require('fs');
 const db = require('./config/database');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5005;
 
 // Middleware
 app.use(cors());
